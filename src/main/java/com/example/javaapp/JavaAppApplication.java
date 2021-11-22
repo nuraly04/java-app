98e4fdc6199e8cfbc97a1e028335dfcc193545b6
nuraly04 <nuralysaparaliev2@gmail.com>
2021-11-22 22:52:28 +0600

    Initial commit

---
+++ src/main/java/com/example/javaapp/JavaAppApplication.java
@@ -0,0 +1,4 @@
+package com.example.javaapp;
+
+public class JavaAppApplication {
+}
---
+++ src/main/java/com/example/javaapp/model/User.java
@@ -0,0 +1,43 @@
+package com.example.javaapp.model;
+
+public class User {
+
+    private int id;
+
+    private String name;
+
+    private int age;
+
+    public User(int id, String name, int age) {
+        this.name = name;
+        this.age = age;
+    }
+
+    public User() {
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+}
